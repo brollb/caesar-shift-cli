8d3b4bb6fd2ff448b68042cee368cad3843df1cb
Brian Broll <brian.broll@gmail.com>
2018-07-04 21:37:53 -0500

    Added stdin pipe support

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,15 @@
 extern crate clap;
+extern crate isatty;
 
 use clap::{Arg,App};
+use std::io::{self, BufRead};
 
 fn main() {
-    let matches = App::new("Caesar Shift CLI")
+    let has_stdin_pipe = !isatty::stdin_isatty();
+
+    let mut app = App::new("Caesar Shift CLI")
         .version("1.0.0")
         .author("Brian Broll")
-        .arg(Arg::with_name("message")
-             .help("Input message to encrypt")
-             .required(true)
-             .index(1)
-         )
         .arg(Arg::with_name("key")
              .help("shift amount to use for the key")
              .takes_value(true)
@@ -22,15 +21,36 @@ fn main() {
              .help("decrypt the input message")
              .short("d")
              .long("decrypt")
-         )
-        .get_matches();
+         );
+
+    if !has_stdin_pipe {
+        app = app.arg(Arg::with_name("message")
+             .help("Input message to encrypt")
+             .required(true)
+             .index(1)
+        );
+    }
+
+    let matches = app.get_matches();
 
     let mut shift = matches.value_of("key").unwrap().parse::<u8>().unwrap() % 26;
     if matches.is_present("decrypt") {
         shift = 26 - shift;
     }
-    let input_text = matches.value_of("message").unwrap();
 
+    if has_stdin_pipe {
+        let stdin = io::stdin();
+        for line in stdin.lock().lines() {
+            let line = line.expect("Could not read line from standard in");
+            encrypt_and_print(&line, shift);
+        }
+    } else {
+        let input_text = matches.value_of("message").unwrap();
+        encrypt_and_print(input_text, shift);
+    }
+}
+
+fn encrypt_and_print(input_text: &str, shift: u8) {
     // convert to a vector of chars and then encrypt each
     let encrypted_chars = input_text.chars()
         .map(|c| {
